ab383817e9934b3d2d2308764b040033beb33bdb
amina-taleb <amina.taleb@laplateforme.io>
2025-01-14 11:41:56 +0100

    add a try-except bloc, correct the functuin structure by adding return at the end in order the cariable result will be recognized, change the printed message in case of zero devision error

--- calcul_prog_fonctionnelle.py
+++ calcul_prog_fonctionnelle.py
@@ -23,29 +23,32 @@ def calculator(a, b, operator) :
         if b != 0 :
             result = a / b
         else :
-            print(f"Erreur : division par zéro")  
+            result = "Zero Division Error ! Enter another number b " 
 
 
     elif operator == '//' :
         if b != 0 :
             result = a // b
         else :
-            print(f"Erreur : division par zéro")
+            result = "Zero Division Error ! Enter another number b "
 
     elif operator == '%' :
         if b != 0 : 
             result = a % b
         else :
-            print(f"Erreur : division par zéro") 
+            result = "Zero Division Error ! Enter another number b " 
 
-    print(f"The result of this operation is :{result}")
+    return print(f"The result of the operation : a {operator} b = {result}")
 
     ##Add the numpy functions : cos, sin, log, exp n! .....
 
 #Ask the use the enter the values and the operator :
+try :
+    number1 = int(input(f"Enter a first number a : "))
+    number2 = int(input(f"Enter a second number b : "))
+    operate = input(f"Enter the operation symbol '+'  '-'  '*'  '/'  '//'  '%' : ")
 
-number1 = int(input(f"Enter a first number a : "))
-number2 = int(input(f"Enter a second number b : "))
-operate = input(f"Enter the operation symbol '+'  '-'  '*'  '/'  '//'  '%' : ")
+    calculator(number1, number2, operate)
 
-operation_result = calculator(number1, number2, operate)
+except ZeroDivisionError :
+    print(f"Erreur : division par zéro")
