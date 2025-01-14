fd5e121fda4278066cb0f3f7f4448166394af300
amina-taleb <amina.taleb@laplateforme.io>
2025-01-14 13:33:04 +0100

    ask the user to choose between to types of functions (trigo and others), add other types of error (valueerror in case of invalide choice and keyboardInterrupt to quit the programm correctly

--- calcul_prog_fonctionnelle.py
+++ calcul_prog_fonctionnelle.py
@@ -36,7 +36,7 @@ def basic_calculator(a, b, operator) :
 
 
 # 2 - Define a scientific calculator due to the library numpy :
-def science_calculator(a, operators) :
+def science_calculator(a, operator) :
     #Here I want to use another method : dictionnary, I will stock my operators in the dictionnary and then call them later
     #The operator will be the key and the operation its value
     operators = {
@@ -64,25 +64,47 @@ def science_calculator(a, operators) :
         'root' : np.sqrt(a) if a >= 0 else 'Error: Negative value below the root',
     }
 
-    return f"The result of this operation is :{operators.get(operators, "opérateur invalide")}"
+    return f"The result of this operation is :{operators.get(operator, "opérateur invalide")}"
+
 
 #Ask the use the enter the values 
 try :
 #Ask the user to choose between two types of operators :
-    print(f"Welcome to our calculator ! ")
-    choice = input(f"Choose the calculator type (basic / scientific) :").strip().lower()
+    print(f"Welcome to our calculator ! \n")
+    choice1 = input(f"Choose the calculator type (basic / scientific) :").strip().lower()
 
-    if choice == 'basic' :
+    if choice1 == 'basic' :
         operate = input(f"Enter the operation symbol '+'  '-'  'x'  '/'  '//'  '%' : ")
         number1 = int(input(f"Enter a first number a : "))
         number2 = int(input(f"Enter a second number b : "))
         basic_calculator(number1, number2, operate)
         
-    elif choice == 'scientific' :
-        number = int(input(f"Enter a number of your choice n : "))
-        operate = input(f"Enter the operation symbol '+'  '-'  'x'  '/'  '//'  '%' : ")
-        science_calculator(number, operate)
+    elif choice1 == 'scientific' :
+        choice2 = input(f"Choose the operator type (trigonometrical / other) :").strip().lower()
+        if choice2 == 'trigonometrical' :
+            operate = input(f"Enter the operation symbol 'cos'  'sin'  'tan'  'arccos'  'arcsin'  'arctan'  'cosh'  'sinh'  'tanh'  'arccosh' 'arcsinh'  'arctanh' : ")
+            number = int(input(f"Enter a number of your choice n : "))
+            science_calculator(number, operate)
+
+        elif choice2 == 'other' :
+            operate = input(f"Enter the operation symbol 'log'  'ln'  'exp'  'abs'  'root' : ")  
+            number = int(input(f"Enter a number of your choice n : "))
+            science_calculator(number, operate)
+
+        else :
+            print(f"Invalide choice. Please enter 'trigonometrical' or 'other")
+
+    else :
+        print(f"Invalide choice. Please enter 'basic' or 'scientific")
 
 except ZeroDivisionError :
     print(f"Erreur : division par zéro")
 
+except ValueError :
+    print("Invalid input. Please enter valid numbers.")
+
+except KeyboardInterrupt :
+    print(f"\nExiting ......")
+
+
+  
