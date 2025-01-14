9dcc0cdc5a998e7908af93355c4a42b2129dfc4e
amina-taleb <amina.taleb@laplateforme.io>
2025-01-14 11:50:51 +0100

    change the star * bu x to improve the printed message for the user

--- calcul_prog_fonctionnelle.py
+++ calcul_prog_fonctionnelle.py
@@ -2,22 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-#initialize the variables (if you want to work with global variables)
-
+########################################################################
 #define the functions :
+# 1 - Define a basic calculator with basic operators : +, x,
 def calculator(a, b, operator) :
     if operator == '+' :
         result = a + b
 
-
     elif operator == '-' :
         result = a - b
 
-
-    elif operator == '*' :
+    elif operator == 'x' :
         result = a * b
-
 
     elif operator == '/' :
         if b != 0 :
@@ -25,13 +21,11 @@ def calculator(a, b, operator) :
         else :
             result = "Zero Division Error ! Enter another number b " 
 
-
     elif operator == '//' :
         if b != 0 :
             result = a // b
         else :
             result = "Zero Division Error ! Enter another number b "
-
     elif operator == '%' :
         if b != 0 : 
             result = a % b
@@ -40,13 +34,23 @@ def calculator(a, b, operator) :
 
     return "The result of the operation : a {operator} b = {result}"
 
-    ##Add the numpy functions : cos, sin, log, exp n! .....
+
+# 2 - Define a scientific calculator due to the library numpy :
+def science_calculator(a, b, opertor_s) :
+    #Here I want to use another method : dictionnary, I will stock my operators in the dictionnary and then call them later
+    #The operator will be the key and the operation its value
+    opertor_s = {
+        'cos' : np.cos(a),
+        'sin' : np.sin(a),
+        'tan' : np.tan(a)
+        'sinh' :
+    }
 
 #Ask the use the enter the values and the operator :
 try :
     number1 = int(input(f"Enter a first number a : "))
     number2 = int(input(f"Enter a second number b : "))
-    operate = input(f"Enter the operation symbol '+'  '-'  '*'  '/'  '//'  '%' : ")
+    operate = input(f"Enter the operation symbol '+'  '-'  'x'  '/'  '//'  '%' : ")
 
     calculator(number1, number2, operate)
 
