6fc397d303182fb3adc2ce6e5de3c6f04eab5c9b
amina-taleb <amina.taleb@laplateforme.io>
2025-01-14 10:09:04 +0100

    create my branch and add the first file

---
+++ calcul_prog_fonctionnelle.py
@@ -0,0 +1,12 @@
+#import the necessary libraries :
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#initialize the variables
+
+#define the functions :
+def sum_(a, b) :
+    return a + b
+
+def soustract()
